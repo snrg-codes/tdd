ef311b5a8e9ef55e376dc6f7cad5e8eefb5556bb
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-06-05 14:38:16 +0500

    by.TAG_NAME

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.by import By
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -10,7 +12,41 @@ class NewVisitorTest(unittest.TestCase):
     
     def test_myTest(self):
         self.browser.get('http://localhost:8000')
-        self.assertIn('successfully!', self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
+        header_text = self.browser.find_elements(By.TAG_NAME, 'h1')[0].text
+        print(header_text)
+        self.assertIn('To-Do lists', header_text)
+        # <h1>To-Do lists</h1>
+
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Bu joyga yangi ishni qo\'shing'
+        )
+
+        inputbox.send_keys("Telefonga pul o'tkazish")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(
+            any(row.text == '1: Telefonga pul o\'tkazish' for row in rows),
+        )
+
+        self.fail('Finish the test!')
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def bosh_sahifa(request):
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    return HttpResponse("<html><title>To-Do lists</title><body><h1>To-Do lists</h1></body></html>")
